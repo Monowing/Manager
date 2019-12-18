1ba126ee1413b0bd1753446588393ad89abda953
Monowing <2410613857@qq.com>
2019-12-18 17:25:44 +0800

    update

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -15,7 +15,7 @@ $(function() {
 	 * 菜单跳转
 	 */
     $(".main_left a").on("click",
-    function() {
+    function(){
         var address = $(this).attr("data-src");
         $("iframe").attr("src", address);
     });
@@ -39,7 +39,7 @@ var myStringUtils = {
 	 * 判断字符串是否为空
 	 */
     isEmpty: function(str) {
-        if (str  ==  ""  ||  str  ==  undefined  ||  str  ==  null) {
+        if (str == "" || str == undefined || str == null){
             return true;
         }
         return false;
@@ -48,11 +48,33 @@ var myStringUtils = {
     /***************************************************************************
 	 * 判断字符串是否为空，或者是否为空格
 	 */
-    inputCheckEmpty(str) { 
-        if (str  ==  ""  ||  str  ==  undefined  ||  str  ==  null ||   (str.length > 0  &&  str.trim().length  ==  0)) {
-            return true;
+    isBlankOrEmpty(str) { 
+    	if (str == "" || str == undefined || str == null || (str.length > 0 && str.trim().length == 0)){
+    		return true;
         }
         return false;
     }
+
+}
+
+var myCommonUtils =	{
+	
+	/***************************************************************************
+	 * 手机号验证
+	 */
+	phoneCheck: function (value){
+		var temp = /^(1[3-9])[0-9]{9}$/;
+		return temp.test(value);
+	},
+	
+	/***************************************************************************
+	 * 邮箱验证
+	 */
+	emailCheck:function (value){
+		var temp = /^[A-Za-z0-9\u4e00-\u9fa5]+@[a-zA-Z0-9_-]+(\.[a-zA-Z0-9_-]+)+$/; 
+		return temp.test(value);
+	}
+		
+
 
 }
